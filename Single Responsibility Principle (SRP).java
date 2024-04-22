c00749681f7edbafebf0ba5ab55fcc0dddc22317
Muhammad Habibur Rahman <159675632+Black-Bird-31@users.noreply.github.com>
2024-04-22 09:58:32 +0600

    Update Single Responsibility Principle (SRP).java

--- Single Responsibility Principle (SRP).java
+++ Single Responsibility Principle (SRP).java
@@ -1,4 +1,11 @@
-// Book class
+/*
+ * Program Title: A JAVA program demonstrating Single Responsibility Prinicple (SRP) of SOLID.
+ * Author: Md. Habibur Rahman, CSEKU.
+ */
+
+import java.util.*;
+
+// Book class with getter and setter methods
 class Book {
     private String title;
     private String author;
@@ -10,31 +17,80 @@ class Book {
         this.yearPublished = yearPublished;
     }
 
-    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
 }
 
-// BookManager class responsible for managing books
+// BookManager class with methods to add or remove books
 class BookManager {
+    private List<Book> books;
+
+    public BookManager() {
+        this.books = new ArrayList<>();
+    }
+
     public void addBook(Book book) {
-        // Code to add book to library
+        books.add(book);
+        System.out.println("Book added: " + book.getTitle());
     }
 
     public void removeBook(Book book) {
-        // Code to remove book from library
+        if (books.remove(book)) {
+            System.out.println("Book removed: " + book.getTitle());
+        } else {
+            System.out.println("Book not found: " + book.getTitle());
+        }
     }
 
-    // Other book management methods
+    public void listBooks() {
+        System.out.println("Books in library:");
+        for (Book book : books) {
+            System.out.println(book.getTitle() + " by " + book.getAuthor() + " (" + book.getYearPublished() + ")");
+        }
+    }
 }
 
-// UserManager class responsible for managing users
-class UserManager {
-    public void addUser(User user) {
-        // Code to add user to library system
-    }
+public class BookLibraryExample {
+    public static void main(String[] args) {
+        // Create BookManager instance
+        BookManager manager = new BookManager();
 
-    public void removeUser(User user) {
-        // Code to remove user from library system
-    }
+        // Create some books
+        Book book1 = new Book("Java Programming", "John Smith", 2020);
+        Book book2 = new Book("Python Basics", "Emily Brown", 2019);
 
-    // Other user management methods
+        // Add books to the library
+        manager.addBook(book1);
+        manager.addBook(book2);
+
+        // List all books in the library
+        manager.listBooks();
+
+        // Remove a book
+        manager.removeBook(book2);
+
+        // List all books again
+        manager.listBooks();
+    }
 }
