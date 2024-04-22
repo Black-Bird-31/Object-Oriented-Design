c8b2af704440c401c3f6cde36ca05eabfccfd15b
Muhammad Habibur Rahman <159675632+Black-Bird-31@users.noreply.github.com>
2024-04-22 10:19:13 +0600

    Update DIP.java

--- DIP.java
+++ DIP.java
@@ -1,3 +1,50 @@
+
+/*
+ * Program Title: A JAVA program demonstrating Dependency Inversion Principle of SOLID.
+ * Author: Md. Habibur Rahman, CSEKU.
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Book class with getter and setter methods
+class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+}
+
 // Interface for LibraryService
 interface LibraryService {
     void addBook(Book book);
@@ -9,11 +56,13 @@ class DatabaseLibraryService implements LibraryService {
     @Override
     public void addBook(Book book) {
         // Code to add book to database
+        System.out.println("Book added to database: " + book.getTitle());
     }
 
     @Override
     public void removeBook(Book book) {
         // Code to remove book from database
+        System.out.println("Book removed from database: " + book.getTitle());
     }
 }
 
@@ -40,5 +89,13 @@ class LibraryManager {
         this.libraryService = libraryService;
     }
 
-    // Methods using LibraryService
+    // Method to add a book using LibraryService
+    public void addBook(Book book) {
+        libraryService.addBook(book);
+    }
+
+    // Method to remove a book using LibraryService
+    public void removeBook(Book book) {
+        libraryService.removeBook(book);
+    }
 }
